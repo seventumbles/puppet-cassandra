5652ec1fe16d3fe2b3a52a8ba773e168f7ddd848
Mike Lazar <michael.g.lazar@monsanto.com>
2014-10-08 11:00:43 -0500

    Add lifecycle to datastax agent

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -4,8 +4,10 @@ class cassandra::agent(
   file { "/var/lib/datastax-agent/conf/address.yaml":
     ensure  => file,
     content => template("${module_name}/address.yaml.erb"),
+    require => Package['dse-full'],
+    notify => Service['datastax-agent']
   }
-  
+
   service { 'datastax-agent':
     ensure     => running,
     enable     => true,
