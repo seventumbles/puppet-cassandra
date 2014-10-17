4e10b3f9374f54e214ed5ab85533027752f484fc
Mike Lazar <michael.g.lazar@monsanto.com>
2014-10-16 19:08:25 -0500

    Implement add or update cluster configs logic in OpsCenter notifier

--- lib/puppet/provider/opscenter_notifier/opscenter_api.rb
+++ lib/puppet/provider/opscenter_notifier/opscenter_api.rb
@@ -30,17 +30,43 @@ Puppet::Type.type(:opscenter_notifier).provide(:opscenter_api) do
   #####
 
   def update_cluster_configs
-    seeds = resource[:seed_nodes].join(',')
-    cluster_configs = {"cassandra" => {"seed_hosts" => "#{seeds}", "api_port" => resource[:api_port]}}.to_json
-    opscenter_url = resource[:opscenter_ip] + '/cluster-configs'
-    opscenter_api('-o', 'result.txt',
-                  '-X',
-                  'POST',
-                  '-H',
-                  '"application/json"',
-                  opscenter_url,
-                  '-d',
-                  cluster_configs)
+    if get_cluster_configs.has_key? resource[:name]
+      update_opscenter_cluster
+    else
+      new_opscenter_cluster
+    end
   end
 
+  def get_cluster_configs
+    opscenter_url = "#{resource[:opscenter_ip]}/cluster-configs"
+    JSON.parse opscenter_api('-s', opscenter_url)
+  end
+
+  def update_opscenter_cluster
+    opscenter_url = "#{resource[:opscenter_ip]}/cluster-configs/#{resource[:name]}"
+    notify_opscenter opscenter_url, 'PUT'
+  end
+
+  def new_opscenter_cluster
+    opscenter_url = "#{resource[:opscenter_ip]}/cluster-configs"
+    new_cluster_name = notify_opscenter opscenter_url, 'POST'
+
+    #OpsCenter handles duplicate posts (made by race conditions) by creating a new cluster name with an autoincrement
+    #number appended (mycluster -> mycluster1 -> mycluster2, etc).
+    #When we lose the race, delete the newly created duplicate
+    new_cluster_name = new_cluster_name.reverse.chomp('"').reverse.chomp('"')
+    if new_cluster_name != resource[:name]
+      delete_duplicate_from_opscenter("#{opscenter_url}/#{new_cluster_name}")
+    end
+  end
+
+  def notify_opscenter(opscenter_url, method)
+    seeds = resource[:seed_nodes].join(',')
+    cluster_configs = {"cassandra" => {"seed_hosts" => "#{seeds}", "api_port" => resource[:api_port]}}.to_json
+    opscenter_api('-s', '-X', method, '-H', '"application/json"', opscenter_url, '-d', cluster_configs)
+  end
+
+  def delete_duplicate_from_opscenter(opscenter_url)
+    opscenter_api('-s', '-X', 'DELETE', opscenter_url)
+  end
 end
